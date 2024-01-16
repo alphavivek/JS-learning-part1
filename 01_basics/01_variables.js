bc12065a55c9396c10615d393a633020e5e63252
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-16 07:52:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+var accountid=3210
+let accountno="584212"
+accountemail="vivek@yahoo.com"
+const accountname="vivek"
+let accountstate
+
+console.log(accountid);
+console.log(accountno);
+console.log(accountemail);
+console.log(accountname);
+
+accountid=111
+accountno=336699
+accountemail="Pathak@google.com"
+
+console.table([accountid,accountno,accountemail,accountname,accountstate])
