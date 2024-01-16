85d643f8ab45facad62cc7c1a2003d935a20a923
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-16 08:42:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let age=22
+let Name="vivek kumar"
+let wbvalue=null
+let state
+
+console.log(age);
+
+console.log(typeof age);
+console.log(typeof Name);
+console.log(typeof wbvalue);
+console.log(typeof state);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (weather example if server can't sent the temp value then value gone to null means current value is showing or not fetched.)
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof null);   // Object type
+console.log(typeof undefined);  // undefined type
