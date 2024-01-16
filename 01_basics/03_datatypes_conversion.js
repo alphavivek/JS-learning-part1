f4e11732490c557cf435b15dc62e52f1db888638
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-16 13:25:35 +0000

    datatypes conversion

---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,31 @@
+let score= false
+
+let num1 = Number(score)
+console.log(num1);
+console.log(typeof (num1));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let num=11
+
+let numconvert=String(num)
+// console.log(numconvert);
+// console.log(typeof numconvert);
+
+let boolnum = true
+
+let isstring = String(boolnum)
+// console.log(isstring);
+// console.log(typeof isstring);
+
+let string1 = 11
+
+let bolre = Boolean(string1)
+// console.log(bolre);
+// console.log(typeof bolre);
+
+// 1 => true; 0 => false
+// "" => false
+// "vivek" => true
