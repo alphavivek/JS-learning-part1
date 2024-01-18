68b99f929a6a437973ca0442cb2c89d11ffbc9ab
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-18 09:10:38 +0000

    data and num

---
+++ 01_basics/06_math_and_num.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// **** maths ****
+
+console.log(Math.abs(-4));
+console.log(Math.round(7.88));
+
+console.log(Math.random());  // the value of random function is lies between 0 and 1.
+console.log((Math.random()*10 ));
+console.log((Math.random()*10 )+1);
+console.log(Math.floor(Math.random()*10 )+1);
+
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1) )+min);
+
