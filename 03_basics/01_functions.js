6f1f81cac5fabdc43b598e36090b097ebf9ef88a
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-25 14:35:06 +0000

    Functions and functions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,3 +36,41 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+
+
+//*********** Functions with Objects ***************
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
