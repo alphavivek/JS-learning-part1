476d17542e62944130bc2ac6aa6f53e7771294ee
Vivek Kumar Pathak <109842993+alphavivek@users.noreply.github.com>
2024-01-25 13:14:47 +0000

    objects and destructuring of objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -56,3 +56,36 @@ console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+//************objects destructuring***********
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+//*********** JSON and API **********
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+// json format objects
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+// json array
